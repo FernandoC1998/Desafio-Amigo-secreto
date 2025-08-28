0efa93046e09034ba966f34dddcb46d99900fe17
Fernando Camarillo <95309900+FernandoC1998@users.noreply.github.com>
2025-08-27 20:17:59 -0600

    Actualizando el app.js

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 let amigos = [];
 
-function agregarNombres(){
-    let nombreNuevo = parseInt(document.getElementById('amigo').value);
-    console.log (nombreNuevo);
+
+function agregarAmigo(){
+    let input = document.getElementById('amigo');
+    let nombreNuevo = input.value.trim();
+     if (nombreNuevo === "") {
+         alert("Por favor, inserte un nombre.");
+         return;
+        }
+        amigos.push(nombreNuevo);
+        actualizarLista();
+        input.value = "";    
 }
 
-agregarNombres();
+function actualizarLista() {
+  const lista = document.getElementById('listaAmigos');
+  lista.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const li = document.createElement('li');
+    li.textContent = amigo;
+    lista.appendChild(li);
+  });
+}
+
+
+function sortearAmigo(){
+    const resultado = document.getElementById('resultado');
+
+  if (amigos.length === 0) {
+    alert("La lista está vacía. Agrega al menos un nombre antes de sortear.");
+    return;
+  }
+
+
+  const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[indiceAleatorio];
+
+
+  resultado.textContent = `El amigo secreto es: ${amigoSorteado} `;
+
+}
+
+agregarAmigo();
